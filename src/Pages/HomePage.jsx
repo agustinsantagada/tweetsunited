3d5a04286387af79b772383971e3727cfe3951e5
Agustin Santagada <agustinsantagada@gmail.com>
2022-01-24 00:45:21 -0300

    ajustes router

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,17 +1,22 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, useHistory } from "react-router-dom";
 import MapTweet from "../Containers/MapTweet"
 import NewTweet from "../Components/NewTweet"
 import UserBar from "../Pages/UserBar"
 import ProfilePage from "../Pages/ProfilePage"
 
+
+
+
 export default function HomePage () {
+  const history = useHistory();
+
     return (
-      <BrowserRouter>
+      <BrowserRouter history={history}>
         <div className="HomePage">
           <Switch>
             <Route component = {UserBar} path = '/home'  exact/>
-            <Route component = {ProfilePage} path = '/ProfilePage'  exact/>
+            <Route component = {ProfilePage} path = '/ProfilePage' exact/>
           </Switch>
             <Route component = {NewTweet} path = '/home'  exact/>
             <Route component = {MapTweet} path = '/home'  exact/>  
--- src/Pages/NickPage.jsx
+++ src/Pages/NickPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { AppContext } from "../Context/AppContext"
 import { useContext } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, useHistory } from "react-router-dom";
 import HomePage from "../Pages/HomePage"
 import NickName from "../Components/NickName"
 import "../Styles/NickPage.css"
@@ -9,9 +9,10 @@ import "../Styles/NickPage.css"
 export default function NickPage () {
 
   const { nickName } = useContext(AppContext);
+  const history = useHistory()
 
   return (
-    <BrowserRouter>
+    <BrowserRouter history={history}>
     <Switch>
       <div className="np-bkg">
       {!nickName ? 
